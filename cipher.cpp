aebacf486de84f7fab4e91da888300169d76944a
Adam <adt6261@gmail.com>
2018-12-28 17:41:29 -0500

    removal of punctuation and uppercase works via two loops. Can explore using one loop. Working on actual shifting process. May need to switch alphabet to operate on deque of chars instead of strings

--- cipher.cpp
+++ cipher.cpp
@@ -1,34 +1,43 @@
 #include <iostream> 
 #include <string>
+#include <deque>
+#include <algorithm>
 #include "alphabet.h"
 
 using namespace std; 
 
-class Cipher { 
+class Cipher {
   public:
 
   string keyword; 
 
   string decipheredText;
 
-  deque<string> encipheredAlphabet;
+  deque<string> regAlphabet;
+
+  deque<string> encipheredAlphabet; 
+
+    static bool is_punctuation (char c) {
+      return c ==  '.' || c == ',' || c == '!' || c == '\''|| c == '?' || c == ' ';
+  }
 
   string encipher(string text) { 
     Alphabet a;
+    regAlphabet = a.getStandard();
     encipheredAlphabet = a.cipherLetters(keyword);
     //to do: encipher each piece of text according to the enciphered alphabet, with the following rules: 
-    // all chars lowercase 
-    // all chars in blocks of five 
+    // all chars lowercase
+    // all chars in blocks of five
     // all punctuation omitted
 
+    text.erase( remove_if(text.begin(), text.end(), is_punctuation), text.end() );
     
     string::iterator it;
-    for (it = text.begin(); it != text.end(); it++) { 
-      if (text[*it] == '.'|| text[*it] == ',' || text[*it] == '!' || text[*it] == '''|| text[*it] == '?' || text[*it] == ' ') {
-        cout << f;
-        text.erase(*it);
-      }
-      f = tolower(f);
+    for (it = text.begin(); it != text.end(); it++) {
+      *it = tolower(*it);
+      deque<string>::iterator pos;
+      pos = find (regAlphabet.begin(), regAlphabet.end(), *it);
+      *it = encipheredAlphabet[pos];
     }
 
     return text;
@@ -58,9 +67,9 @@ int main() {
 
   string encipheredText = c.encipher(c.decipheredText); 
 
-  // for (auto f: encipheredText) { 
-  //   cout << f;
-  // }
+  for (auto f: encipheredText) { 
+    cout << f;
+  }
 
   return 0;
 
